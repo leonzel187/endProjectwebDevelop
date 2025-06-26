af2d169b976f361b97cbe6c230b35ac96a06fa12
Ori Shriki <ori.shriki@gmail.com>
2025-06-26 18:31:57 +0300

    made more progess on intercativeForm. Added TODOs for whats need next

---
+++ jScripts/interactiveForm.js
@@ -0,0 +1,9 @@
+function setSize(size) {
+    
+}
+
+function setFood(size) {
+
+}
+
+//TODO: Add event listener to id=dogName textbox - https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
